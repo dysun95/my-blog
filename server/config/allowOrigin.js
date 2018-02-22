1b75ffcf52576a9fe597c6f816147642e805f73c
dysun95 <dysun95@qq.com>
2018-02-22 23:40:24 +0800

    add origin

--- server/config/allowOrigin.js
+++ server/config/allowOrigin.js
@@ -1,5 +1,7 @@
 module.exports = [
   'http://localhost:8080',
   'http://local.dysun95.tk',
-  'http://local.dysun95.tk:8080'
+  'http://local.dysun95.tk:8080',
+  'http://www.dysun95.tk',
+  'https://www.dysun95.tk'
 ]
