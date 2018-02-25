9594c8d9567ea7aad6068a8843ec788f68267c59
dysun95 <dysun95@qq.com>
2018-02-25 18:00:13 +0800

    modify githook listen

--- githook.js
+++ githook.js
@@ -5,12 +5,35 @@ const util = require('util')
 const execFile = util.promisify(require('child_process').execFile)
 
 router.post('/git/hook', async (ctx) => {
-  const { stdout } = await execFile('/root/project/my-blog/run.sh')
-  console.log(stdout)
-  ctx.body = {
-    status: 200,
-    data: stdout
+  if (ctx.request.body.commits && serverChanged(ctx.request.body.commits)) {
+    const { stdout } = await execFile('/root/project/my-blog/run.sh')
+    console.log(stdout)
+    ctx.body = {
+      status: 200,
+      message: 'server had restart',
+      data: stdout
+    }
+  } else {
+    ctx.body = {
+      status: 200,
+      message: 'push has received'
+    }
   }
 })
+
+function serverChanged (commits) {
+  let {added, removed, modified} = commits
+  let all = [added, removed, modified]
+  all.forEach(array => {
+    if (array && array.length > 0) {
+      array.forEach(item => {
+        if (item.match(/^server\//)) {
+          return true
+        }
+      })
+    }
+  })
+  return false
+}
 
 module.exports = router
